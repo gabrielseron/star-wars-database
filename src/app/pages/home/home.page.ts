52aad9f2ddb0e8e9d5dc67e49cd690a2a6672876
gabrielseron <gabriel.seron.pro@gmail.com>
2021-12-11 17:46:43 +0100

    open character modal

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController, LoadingController } from '@ionic/angular';
+import { ModalController, LoadingController, Platform } from '@ionic/angular';
 import { SplashScreen } from '@capacitor/splash-screen';
 import { SwapiService } from '../../services/swapi.service';
 import { Characters } from '../../interfaces/characters';
 import { LanguageComponent } from '../../modal/language/language.component';
 import { CharacterComponent } from '../../modal/character/character.component';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 @Component({
   selector: 'app-home',
@@ -20,6 +21,8 @@ export class HomePage implements OnInit {
     private swapi: SwapiService,
     private modal: ModalController,
     private loading: LoadingController,
+    private storage: NativeStorage,
+    private platform: Platform
   ) { }
 
   async ngOnInit()
@@ -41,6 +44,23 @@ export class HomePage implements OnInit {
     this.swapi.getCharacter(url).then(async(data: Characters) =>
     {
       console.log(data);
+      if (this.platform.is("desktop"))
+      {
+        if (!localStorage.getItem(data.name))
+          await localStorage.setItem(data.name, JSON.stringify(data))
+      } else
+      {
+        if (!this.storage.getItem(data.name))
+          await this.storage.setItem(data.name, JSON.stringify(data))
+      }
+      const modal = await this.modal.create(
+      {
+        component: CharacterComponent,
+        componentProps: {
+          'characterName': data.name,
+        }
+      })
+      return await modal.present()
     })
   }
 
