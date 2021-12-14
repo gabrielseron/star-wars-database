9cb2eca5fbd8cc6c5a7e46c48259a9a7c33c264f
gabrielseron <gabriel.seron.pro@gmail.com>
2021-12-14 17:57:40 +0100

    begin adding popover / data reset / change language

--- src/app/modal/language/language.component.ts
+++ src/app/modal/language/language.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-
+import { ModalController, Platform, PopoverController } from '@ionic/angular';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
+import { WarningResetComponent } from '../../popovers/warning-reset/warning-reset.component';
 @Component({
   selector: 'app-language',
   templateUrl: './language.component.html',
@@ -8,12 +9,23 @@ import { ModalController } from '@ionic/angular';
 })
 export class LanguageComponent implements OnInit {
 
+  selectedLanguage: string
+
   constructor
   (
     private modal: ModalController,
+    private storage: NativeStorage,
+    private platform: Platform,
+    private popover: PopoverController,
   ) { }
 
-  ngOnInit() {}
+  async ngOnInit()
+  {
+    if (this.platform.is("desktop"))
+      this.selectedLanguage = localStorage.getItem("Language")
+    else
+      this.selectedLanguage = await this.storage.getItem("Language")
+  }
 
   close()
   {
@@ -21,5 +33,44 @@ export class LanguageComponent implements OnInit {
     {
       'dismissed': true
     })
+  } 
+
+  changeLanguage(language: string)
+  {
+    if (language == 'en')
+    {
+      if (this.platform.is("desktop"))
+      {
+        localStorage.setItem("Language", "en")
+      }else
+      {
+        this.storage.setItem("Language", "en")
+      }
+      this.selectedLanguage = "en"
+    }
+    else
+    {
+      if (this.platform.is("desktop"))
+      {
+        localStorage.setItem("Language", "?format=wookiee")
+      }else
+      {
+        this.storage.setItem("Language", "?format=wookiee")
+      }
+      this.selectedLanguage = "?format=wookiee"
+    }
+    console.log(this.selectedLanguage);
+  }
+
+  async modalClearData()
+  {
+    console.log("click");
+    const popover = await this.popover.create(
+    {
+      component: WarningResetComponent,
+      translucent: true
+    }
+    )
+    return await popover.present();
   }
 }
